4eb23e32c133189de8d06f781c644172c617e7ca
Eduardo San Martin Morote <posva13@gmail.com>
2018-07-02 11:48:07 +0200

    update only lerna-dependent

--- packages/@posva/lerna-dependent/index.js
+++ packages/@posva/lerna-dependent/index.js
@@ -2,5 +2,6 @@ const lernaA = require('@posva/lerna-a')
 const lernaB = require('@posva/lerna-b')
 
 console.log('Hello, I depend on both packages')
+// only I, am updated
 console.log(lernaA)
 console.log(lernaB)
