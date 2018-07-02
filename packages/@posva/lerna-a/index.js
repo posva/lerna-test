9bfc3e633bdbf5ded1092be0abbc12b96682e3fd
Eduardo San Martin Morote <posva13@gmail.com>
2018-07-02 11:19:18 +0200

    add some code to packages, run lerna bootstrap

--- packages/@posva/lerna-a/index.js
+++ packages/@posva/lerna-a/index.js
@@ -1,1 +1,6 @@
+const cows = require('cows')()
+
+console.log('Hello from module A')
+console.log(cows[Math.floor(Math.random() * cows.length)])
+
 module.exports = 'I am module A'
--- packages/@posva/lerna-b/index.js
+++ packages/@posva/lerna-b/index.js
@@ -1,1 +1,6 @@
+const cows = require('cows')()
+
+console.log('Hello from module B')
+console.log(cows[Math.floor(Math.random() * cows.length)])
+
 module.exports = 'I am module B'
